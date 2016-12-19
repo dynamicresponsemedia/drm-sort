4e607ce3aab3ac74fc8aef4d628642339a2ebf9e
Elizabeth Rogers <beth3346@gmail.com>
2016-12-19 14:21:17 -0600

    add elr-ui

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# elr-sort 
+# elr-sort
+
+[![Build Status](https://travis-ci.org/Beth3346/elr-sort.svg?branch=master)](https://travis-ci.org/Beth3346/elr-sort)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![codecov](https://codecov.io/gh/Beth3346/elr-sort/branch/master/graph/badge.svg)](https://codecov.io/gh/Beth3346/elr-sort)
+[![npm](https://img.shields.io/npm/dm/elr-sort.svg?style=flat)]()
 
 list sorting
 
---
+++ src/main.js
@@ -0,0 +1,162 @@
+import elrUtlities from 'elr-utility-lib';
+import elrUI from 'elr-ui';
+const $ = require('jquery');
+
+let elr = elrUtlities();
+let ui = elrUI();
+
+const elrSort = function({
+    listsClass = 'elr-sortable',
+    autoSort = true,
+    buttonClass = 'elr-sort-button',
+    activeClass = 'active',
+    ignoreWords = ['a', 'the']
+} = {}) {
+    const sortButton = `.${buttonClass}`;
+    const self = {
+        sortTime($items, dir) {
+            const sort = (a, b) => {
+                const time1 = elr.patterns.sortTime.exec(elr.trim($(a).text()))[0];
+                const time2 = elr.patterns.sortTime.exec(elr.trim($(b).text()))[0];
+
+                if (elr.isTime(elr.trim($(a).text())) && elr.isTime(elr.trim($(b).text()))) {
+                    a = new Date(`04-22-2014 ${elr.parseTime(time1)}`);
+                    b = new Date(`04-22-2014 ${elr.parseTime(time2)}`);
+                }
+
+                return elr.sortValues(a, b, dir);
+            };
+
+            return $items.sort(sort);
+        },
+        sortDate($items, dir) {
+            const sort = (a, b) => {
+                if (elr.isDate(elr.trim($(a).text())) && elr.isDate(elr.trim($(b).text()))) {
+                    a = new Date(elr.patterns.sortMonthDayYear.exec(elr.trim($(a).text())));
+                    b = new Date(elr.patterns.sortMonthDayYear.exec(elr.trim($(b).text())));
+                }
+
+                return elr.sortValues(a, b, dir);
+            };
+
+            return $items.sort(sort);
+        },
+        sortAlpha($items, dir) {
+            const sort = (a, b) => {
+                a = elr.cleanAlpha(elr.trim($(a).text()), ['the', 'a']).toLowerCase();
+                b = elr.cleanAlpha(elr.trim($(b).text()), ['the', 'a']).toLowerCase();
+
+                return elr.sortValues(a, b, dir);
+            };
+
+            return $items.sort(sort);
+        },
+        sortNumber($items, dir) {
+            const sort = (a, b) => {
+                a = parseFloat(elr.trim($(a).text()));
+                b = parseFloat(elr.trim($(b).text()));
+
+                return elr.sortValues(a, b, dir);
+            };
+
+            return $items.sort(sort);
+        },
+        sortComplexList(types, listItems, dir = 'ascending') {
+            // create sortList arrays
+            const sortLists = elr.createArrays(types);
+
+            // add list items to sortLists arrays
+
+            elr.each(types, function() {
+                const type = this;
+
+                elr.each(listItems, function() {
+                    const $listItem = $(this);
+                    const val = elr.trim($listItem.text());
+
+                    if (elr[`is${elr.capitalize(type)}`](val)) {
+                        sortLists[type].push($listItem);
+                    } else {
+                        return;
+                    }
+                });
+
+                elr.each(sortLists[type], function() {
+                    const val = ($(this).text());
+
+                    $(listItems).each(function(k) {
+                        const listVal = $(this).text();
+
+                        if (listVal === val) {
+                            listItems.splice(k, 1);
+                        }
+                    });
+                });
+            });
+
+            // sort sortLists arrays
+            elr.each(sortLists, function(key) {
+                elr[`sort${elr.capitalize(key)}`](sortLists[key], dir);
+            });
+
+            return elr.concatArrays(sortLists);
+        },
+        sortList(direction, $listItems) {
+            const type = $listItems.parent().data('type');
+            let types;
+
+            if (type) {
+                types = [type];
+            } else {
+                types = ['date', 'time', 'number', 'alpha'];
+            }
+
+            return this.sortComplexList(types, $listItems, direction);
+        },
+        renderSort(sortedList, $list) {
+            $list.empty();
+
+            $.each(sortedList, function() {
+                const value = elr.trim($(this).text());
+                const $listItem = ui.createElement('li', {
+                    text: value
+                });
+
+                $list.append($listItem);
+            });
+        }
+    };
+
+    const toggleActiveClass = function(className, parent) {
+        $(this).closest(parent).find(`.${className}`).removeClass(className).end().end().addClass(className);
+    };
+
+    const $lists = $(`.${listsClass}`);
+
+    if (autoSort) {
+        $.each($lists, function() {
+            const $list = $(this);
+            const $listItems = $list.find('li');
+            const $sortedList = self.sortList('ascending', $listItems);
+
+            self.renderSort($sortedList, $list);
+            $(`button.${buttonClass}[data-sort='ascending']`).addClass(activeClass);
+        });
+    }
+
+    $('body').on('click', sortButton, function() {
+        const $that = $(this);
+        const listId = $that.data('list');
+        const $list = $(`ul#${listId}`);
+        const direction = $that.data('sort');
+        const $listItems = $list.find('li');
+        const $sortedList = self.sortList(direction, $listItems);
+
+        self.renderSort($sortedList, $list);
+        toggleActiveClass.call(this, 'active', '.button-group');
+    });
+
+    return self;
+};
+
+export default elrSort;
